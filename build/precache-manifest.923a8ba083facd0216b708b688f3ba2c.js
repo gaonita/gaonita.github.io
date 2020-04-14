f4326e1236461d9d1255a24880ec16005f115b7a
gaonita <gaon.yg@gmail.com>
2020-04-14 21:56:55 +0200

    index html test

---
+++ build/precache-manifest.923a8ba083facd0216b708b688f3ba2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e95b1b340a330899220605a6e8f025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f07ed5f87284a1a981",
+    "url": "/static/css/main.24757139.chunk.css"
+  },
+  {
+    "revision": "3671f764fb164528c67d",
+    "url": "/static/js/2.a12035ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a12035ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f07ed5f87284a1a981",
+    "url": "/static/js/main.78601ac7.chunk.js"
+  },
+  {
+    "revision": "95f40e7a1b55d525ef44",
+    "url": "/static/js/runtime-main.2e526471.js"
+  }
+]);
