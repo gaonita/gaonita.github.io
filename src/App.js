961c629af69bf4cc650bde0ed3c219ab4989d37c
gaonita <gaon.yg@gmail.com>
2021-05-22 16:50:38 +0200

    only div

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import Intro from "./Intro/Intro";
-import Work from "./components/work";
-import Footer from "./components/footer/footer";
-import Navbar from "./Nav";
 
 function App() {
     return (
         <div className="App">
-            {/*<Navbar/>*/}
-            {/*<Intro/>*/}
-            {/*<Work/>*/}
-            <Footer/>
+            <p>Hi</p>
         </div>
     );
 }
